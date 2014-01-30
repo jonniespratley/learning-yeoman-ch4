2daa030ee0db52b4f38e750b6ec6f13675506eb8
Jonnie Spratley <jonniespratley@gmail.com>
2014-01-29 20:58:45 -0800

    removed image min

--- Gruntfile.js
+++ Gruntfile.js
@@ -327,7 +327,7 @@ module.exports = function (grunt) {
         'jst',
         'useminPrepare',
         'requirejs',
-        'imagemin',
+        //'imagemin',
         'htmlmin',
         'concat',
         'cssmin',
